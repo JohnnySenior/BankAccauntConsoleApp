4b0ebf9a3516f720053934e3f06c8d2dad0bb18b
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-16 00:21:11 +0900

    refactor codes

--- Classes/SecuritySystem.cs
+++ Classes/SecuritySystem.cs
@@ -3,14 +3,29 @@ using System;
 public class SecuritySystem
 {
     public string Password { get; set; }
+
     public void PasswordError()
     {
         Console.WriteLine("Password is wrong, check and try again");
         Console.WriteLine();
     }
+
     public void PasswordPassed()
     {
         Console.WriteLine("Password successfully passed !!!");
         Console.WriteLine();
+    }
+
+    public void CheckPassword()
+    {
+        key: Console.Write("Password:");
+        var password = Console.ReadLine();
+        Console.Clear();
+        if (password != "password")
+        {
+            PasswordError();
+            goto key;
+        }
+        PasswordPassed();
     }
 }
